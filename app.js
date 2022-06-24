7849f0a65ee427a51a87c7640535d74844e5c7c2
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-06-24 02:02:01 -0500

    se agrega boton y estilos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hola mundo")
+const inputColor = document.querySelector("#inputColor")
+const btnMostrar = document.querySelector("#btn-mostrar")
+const colorParrafo = document.querySelector("#color")
+const cajaColor = document.querySelector("#caja")
+console.log(inputColor.value)
+
+btnMostrar.addEventListener("click", () => {
+    // console.log("click"); probar el boton en la consola
+    console.log(inputColor.value)
+    colorParrafo.textContent = inputColor.value
+    cajaColor.style.backgroundColor = inputColor.value
+    navigator.clipboard
+        .writeText(inputColor.value)
+        .then(() => console.log("texto copiado"))
+        .catch((e) => console.log(e));
+});
