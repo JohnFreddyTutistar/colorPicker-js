0cbab854f43406befd63f04312c0a05fab54d0fa
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-06-24 15:38:32 -0500

    se agrega botones de estilos de vista

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const inputColor = document.querySelector("#inputColor")
 const btnMostrar = document.querySelector("#btn-mostrar")
 const colorParrafo = document.querySelector("#color")
 const cajaColor = document.querySelector("#caja")
+const modoOscuro = document.querySelector("#dark")
+const fondoOscuro = document.querySelector("#fondoColor")
+const titulo = document.querySelector("#titulo")
+const botonLight = document.querySelector("#light")
+const parrafoHex = document.querySelector("#color")
 console.log(inputColor.value)
 
 btnMostrar.addEventListener("click", () => {
@@ -13,4 +18,16 @@ btnMostrar.addEventListener("click", () => {
         .writeText(inputColor.value)
         .then(() => console.log("texto copiado"))
         .catch((e) => console.log(e));
+});
+
+modoOscuro.addEventListener("click", () => {
+    fondoOscuro.style.backgroundColor = "#000"
+    titulo.style.color = "#fff"
+    parrafoHex.style.color = "#fff"
+})
+
+botonLight.addEventListener("click", () => {
+    fondoOscuro.style.backgroundColor = "#fff"
+    titulo.style.color = "#000"
+    parrafoHex.style.color = "#000"
 });
